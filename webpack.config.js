f5868e729001564c2be61024891c06e83312fe7f
Betty Diegel <bdiegel@gmail.com>
2021-06-27 18:21:27 -0400

    use html-webpack-plugin to generate a new index file in dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    }
-}
+  entry: "./src/client/index.js",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  // generates a new index html file in dist
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "/src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
