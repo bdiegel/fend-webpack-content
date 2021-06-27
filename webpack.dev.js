d01703bfe0fc976807afa1d0b43927bcb6c8d467
Betty Diegel <bdiegel@gmail.com>
2021-06-27 18:42:56 -0400

    define production and development specfic webpack configurations

---
+++ webpack.dev.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/client/index.js",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  // generates a new index html file in dist
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "/src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
